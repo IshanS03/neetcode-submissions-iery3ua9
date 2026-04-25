a3ac2ee8f60f5ccc6bb6f8c6fbb03eae9c23859b
Ishan Sultan <104545031+IshanS03@users.noreply.github.com>
2026-04-25 11:59:06 -0500

    Add: search-for-word - submission-1

---
+++ Data Structures & Algorithms/search-for-word/submission-1.py
@@ -0,0 +1,35 @@
+class Solution:
+    def exist(self, board: List[List[str]], word: str) -> bool:
+        rows, cols = len(board), len(board[0])
+        
+        def backtrack(i, j, k):
+            # matched the whole word
+            if k == len(word):
+                return True
+            
+            # out of bounds or character mismatch (also catches visited cells marked as '#')
+            if i < 0 or i >= rows or j < 0 or j >= cols or board[i][j] != word[k]:
+                return False
+            
+            # mark current cell as visited
+            temp = board[i][j]
+            board[i][j] = '#'
+            
+            # explore 4 neighbors
+            found = (backtrack(i + 1, j, k + 1) or
+                     backtrack(i - 1, j, k + 1) or
+                     backtrack(i, j + 1, k + 1) or
+                     backtrack(i, j - 1, k + 1))
+            
+            # restore cell
+            board[i][j] = temp
+            
+            return found
+        
+        for i in range(rows):
+            for j in range(cols):
+                if backtrack(i, j, 0):
+                    return True
+        return False
+
+        
